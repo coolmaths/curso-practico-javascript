ee0ea77ac370c520215055b43ef8bc58b312da04
Abraham Morales <ingabrahammorales@gmail.com>
2022-06-25 15:27:40 -0500

    Se realiza la integración de html y css, se mejoran los formatos de presentación responsive

--- figuras.js
+++ figuras.js
@@ -18,7 +18,7 @@ console.groupEnd();
 console.group("triángulos")
 
 function perimetroTriangulo(lado1,lado2,base){
-    return lado1+lado2+base;
+    return (lado1+lado2+base);
 }
 function areaTriangulo(base,altura){
     return (base*altura)/2;
@@ -46,3 +46,48 @@ function areaCirculo(radio){
 }
 
 console.groupEnd();
+
+//aqui interactuamos con html
+function calculaPerimetroDelCuadrado(){
+    const input=document.getElementById("InputCuadrado");
+    const value=input.value;
+    const perimetro=perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calculaAreaDelCuadrado(){
+    const input=document.getElementById("InputCuadrado");
+    const value=input.value;
+    const area=areaCuadrado(value);
+    alert(area);
+}
+function calculaPerimetroDelTriangulo(){
+    const lado1=document.getElementById("InputLado1");
+    const value1=parseFloat(lado1.value);
+    const lado2=document.getElementById("InputLado2");
+    const value2=parseFloat(lado2.value);
+    const base=document.getElementById("InputBase");
+    const value3=parseFloat(base.value);
+    const perimetro=perimetroTriangulo(value1,value2,value3);
+    alert(perimetro);
+}
+function calculaAreaDelTriangulo(){
+    const base=document.getElementById("InputBase");
+    const value1=base.value;
+    const altura=document.getElementById("InputAltura");
+    const value2=altura.value;
+    const area=areaTriangulo(value1,value2);
+    alert(area);
+}
+function calculaPerimetroDelCirculo(){
+    const radio=document.getElementById("InputRadio");
+    const value=radio.value;
+    const perimetro=perimetroCirculo(value);
+    alert(perimetro)
+}
+function calculaAreaDelCirculo(){
+    const radio=document.getElementById("InputRadio");
+    const value=radio.value;
+    const area=areaCirculo(value);
+    alert(area)
+}
